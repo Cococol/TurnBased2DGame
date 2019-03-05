71f58b74bc8bd0268faf5abc3da693e3965dd3b0
cococol <cococol2000@gmail.com>
2019-03-05 18:27:56 +0100

    no message

---
+++ Assets/Editor/FocusHierarchy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditor;
+using UnityEditor.UI;
+
+using UnityEngine;
+
+public class FocusHierarchy : Editor
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
